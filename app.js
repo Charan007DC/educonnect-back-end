f4f1ba08a78163b68a1df2b56b087d37f0354dce
Athidh Vasuj <athidhvasuj123@gmail.com>
2025-07-19 23:26:20 +0530

    give me my sleeeep

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const studentRoutes = require('./routes/studentRoutes');
 const alumniRoutes = require('./routes/alumniRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const path = require('path');
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/student', studentRoutes);
 app.use('/api/alumni', alumniRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/uploads',studentRoutes)
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 module.exports = app;
