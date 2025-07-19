beca6e914848affc2fdf7c03daec2f1e324befdb
Athidh Vasuj <athidhvasuj123@gmail.com>
2025-07-20 00:41:51 +0530

    dot env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const dotenv = require('dotenv');
 const studentRoutes = require('./routes/studentRoutes');
 const alumniRoutes = require('./routes/alumniRoutes');
 const adminRoutes = require('./routes/adminRoutes');
