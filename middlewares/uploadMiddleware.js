48aa88b3ce2d18717bb3dca31c3dab6bc6937ffa
Athidh Vasuj <athidhvasuj123@gmail.com>
2025-07-19 23:41:53 +0530

    fuck cloudinary

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,14 +1,6 @@
-// routes/studentRoutes.js
-const express = require('express');
-const router = express.Router();
-const studentController = require('../controllers/studentController');
-const { verifyStudentToken } = require('../middlewares/verifyStudentToken');
 const multer = require('multer');
 const { storage } = require('../config/cloudinary');
 
 const upload = multer({ storage });
 
-// Route to upload profile picture
-router.put('/upload-profile', verifyStudentToken, upload.single('photo'), studentController.updateProfilePicture);
-
-module.exports = router;
+module.exports = upload;
