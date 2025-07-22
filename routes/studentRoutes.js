f3c4a2390a352a48a6ba968473b8f037b1ad24af
athidh <athidhvasuj123@gmail.com>
2025-07-22 11:43:22 +0530

    update and get student profile

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { registerStudent, loginStudent } = require('../controllers/studentController');
+const {registerStudent,loginStudent,updateProfilePicture,updateStudentProfile,getStudentProfile} = require('../controllers/studentController');
 const upload = require('../middlewares/uploadMiddleware');
-const { updateProfilePicture } = require('../controllers/studentController');
 const verifyStudentToken = require('../middlewares/verifyStudentToken');
+
 // Register route
 router.post('/register', registerStudent);
+
 // Login route
 router.post('/login', loginStudent);
+
 // Update profile picture route
-router.post('/upload-photo',verifyStudentToken,upload.single('photo'), updateProfilePicture);
+router.post('/upload-photo', verifyStudentToken, upload.single('photo'), updateProfilePicture);
+
+// Get student profile (for viewing/editing)
+router.get('/profile', verifyStudentToken, getStudentProfile);
+
+// Update student profile (after editing)
+router.put('/profile', verifyStudentToken, updateStudentProfile);
+
 module.exports = router;
