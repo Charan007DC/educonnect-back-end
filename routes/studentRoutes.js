3fe2f4fed730212cc441a9c6e5d1b19e0a7317cd
RogithBalu <rogithbaluanu@gmail.com>
2025-08-01 11:30:10 +0530

    Email Config

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,6 +4,8 @@ const { registerStudent, loginStudent } = require('../controllers/studentControl
 const upload = require('../middlewares/uploadMiddleware');
 const { updateProfilePicture } = require('../controllers/studentController');
 const verifyStudentToken = require('../middlewares/verifyStudentToken');
+const { requestPasswordReset, resetPassword } = require('../controllers/studentController');
+
 // Register route
 router.post('/register', registerStudent);
 // Login route
@@ -11,3 +13,6 @@ router.post('/login', loginStudent);
 // Update profile picture route
 router.post('/upload-photo',verifyStudentToken,upload.single('photo'), updateProfilePicture);
 module.exports = router;
+
+router.post('/forgot-password', requestPasswordReset);
+router.post('/reset-password', resetPassword);
