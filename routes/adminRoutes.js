2e408a1d4e55c60a917cf8a8bd2217f77757333a
athidh <athidhvasuj123@gmail.com>
2025-08-04 14:41:36 +0530

    trying

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,12 +2,6 @@ console.log("Loading: adminRoutes.js");
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-
-// Only login route
 router.post('/login', adminController.loginAdmin);
-
-module.exports = router;
-router.post('/login', adminController.loginAdmin);
-router.get('/dashboard', adminController.getAdminDashboard);
-
+router.get('/dashboard', adminController.getAdminDashboard)
 module.exports = router;
